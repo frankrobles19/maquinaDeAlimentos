2a7f00fa7d516d63ecc45e4f3bf45c4ace38aa23
frank robles <136114992+frankrobles19@users.noreply.github.com.>
2024-02-14 16:18:32 -0500

    feat: :sparkles: finalizacion del proyecto
    finalice el proyecto, con requerimientos solicitados
    
    @CampusLands

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-console.log("hola mundo")
+let productos = {'A': 270, 'B': 340, 'C': 390};
+
+let monedas = [100, 50, 10];
+
+let producto_elegido = prompt("Elija un producto (A, B, C): ");
+
+if (producto_elegido in productos) {
+    let monto = 0;
+    while (monto < productos[producto_elegido]) {
+        let moneda = parseInt(prompt("Ingrese una moneda (10, 50, 100): "));
+        if (monedas.includes(moneda)) {
+            monto += moneda;
+        } else {
+            alert("Moneda no aceptada, Intente de nuevo");
+        }
+    }
+    let cambio = monto - productos[producto_elegido];
+    if (cambio > 0) {
+        alert("Su cambio es: ");
+
+        for (let i = 0; i < monedas.length; i++) {
+            while (cambio >= monedas[i]) {
+                alert(monedas[i]);
+                cambio -= monedas[i];
+            }
+        }
+    }
+} else {
+    alert("Producto no disponible");
+}
